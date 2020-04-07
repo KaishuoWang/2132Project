03a2847b75144c327cb7bb47d4ba2c2114ef2e5d
RalphHuo <whuo026@uottawa.ca>
2020-04-07 18:06:01 -0400

    guest

--- WebContent/loginSuccessGuest.jsp
+++ WebContent/loginSuccessGuest.jsp
@@ -8,5 +8,16 @@
 </head>
 <body>
 	<h1>Welcome <%=request.getParameter("name") %>, you have login as a guest</h1>
+	<p>you can wether choose to book a hotel or make a rate<p>
+	
+	<form action="book" method="post">
+	<button type="submit">book</button>
+	</form>
+	
+	<form action="rate" method="post">
+	<button type="submit">rate</button>
+	</form>
+
+	
 </body>
 </html>
---
+++ src/servLet/bookServlet.java
@@ -0,0 +1,27 @@
+package servLet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.DBHandler;
+@WebServlet(urlPatterns = "/book")
+
+public class bookServlet extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		DBHandler dbHandler = new DBHandler();
+		resp.sendRedirect("bookHotel.jsp");
+		dbHandler.closeDB();
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+}
