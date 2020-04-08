be2722b87a8066e366a7b2a853859534fce5dab3
Kaishuo Wang <kwang126@uottawa.ca>
2020-04-08 17:22:38 -0400

    e

--- src/servLet/finishPaymentServlet.java
+++ src/servLet/finishPaymentServlet.java
@@ -19,9 +19,7 @@ public class finishPaymentServlet extends HttpServlet{
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		DBHandler dbHandler = new DBHandler();
-<<<<<<< HEAD
 		req.getRequestDispatcher("/lastPage.jsp").forward(req, resp);
-=======
 		if(!dbHandler.findProperty(Integer.parseInt(req.getParameter("id")))){
 			resp.sendRedirect("alreadyBooked.html");
 		}else {
@@ -44,8 +42,6 @@ public class finishPaymentServlet extends HttpServlet{
 				}
 			dbHandler.insertePayment(payment);
 		}
-=======
->>>>>>> 5ad1e92158730f17c1fcf1906a345566a48ed95c
 		Payment payment = new Payment(Integer.parseInt(req.getParameter("paymentID")),Integer.parseInt(req.getParameter("hostID")),req.getParameter("paymentType"),Float.parseFloat(req.getParameter("amount")), req.getParameter("status"));
 		if(req.getParameter("paymentID").equals("")) {
 			payment.setPaymentID(0);
@@ -63,12 +59,7 @@ public class finishPaymentServlet extends HttpServlet{
 			payment.setStatus(null);
 		}
 		dbHandler.insertePayment(payment);
-<<<<<<< HEAD
-		
-=======
 		req.getRequestDispatcher("/lastPage.jsp").forward(req, resp);
->>>>>>> 1dcdf7afd77cdb088e934b37030d487cbaf186b1
->>>>>>> 5ad1e92158730f17c1fcf1906a345566a48ed95c
 		dbHandler.closeDB();
 	}
 	
