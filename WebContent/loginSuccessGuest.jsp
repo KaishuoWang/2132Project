ec64a331f8b5e2eb862442a91f922c0d7453ef1d
RalphHuo <whuo026@uottawa.ca>
2020-04-07 23:14:36 -0400

    guest

--- WebContent/loginSuccessGuest.jsp
+++ WebContent/loginSuccessGuest.jsp
@@ -8,7 +8,7 @@
 </head>
 <body>
 	<h1>Welcome <%=request.getParameter("name") %>, you have login as a guest</h1>
-	<p>you can wether choose to book a hotel or make a rate<p>
+	<p>you can choose to book a hotel or make a rate<p>
 	
 	<form action="book" method="post">
 	<button type="submit">book</button>
@@ -17,7 +17,5 @@
 	<form action="rate" method="post">
 	<button type="submit">rate</button>
 	</form>
-
-	
 </body>
 </html>
---
+++ src/servLet/rateServlet.java
@@ -0,0 +1,29 @@
+package servLet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.DBHandler;
+
+@WebServlet(urlPatterns = "/rate")
+public class rateServlet extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		DBHandler dbHandler = new DBHandler();
+		resp.sendRedirect("rate.jsp");
+		dbHandler.closeDB();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+}
