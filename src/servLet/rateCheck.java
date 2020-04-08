f3d55a78133f6bdce6f3241c856459c6075c3048
RalphHuo <whuo026@uottawa.ca>
2020-04-08 03:27:59 -0400

    guest

--- src/servLet/rateCheck.java
+++ src/servLet/rateCheck.java
@@ -11,17 +11,13 @@ import javax.servlet.http.HttpServletResponse;
 import dao.DBHandler;
 
 @WebServlet(urlPatterns = "/review")
-<<<<<<< HEAD:src/servLet/rateCheck.java
+
 public class rateCheck extends HttpServlet{
-	
-=======
-public class rateServlet extends HttpServlet{
->>>>>>> b84e9daaaa611645847fef1b226211f182bdbbca:src/servLet/rateServlet.java
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		DBHandler dbHandler = new DBHandler();
-		System.out.println("===============");
 		req.getRequestDispatcher("/bookSuccess.jsp").forward(req, resp);
 		dbHandler.closeDB();
 	}
