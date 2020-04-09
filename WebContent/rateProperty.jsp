69a4b27b0f06e9b2624df6ee5e1af84b297717e4
RalphHuo <whuo026@uottawa.ca>
2020-04-08 21:09:36 -0400

    guest

---
+++ WebContent/rateProperty.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="checkIDAva" method="post">
+		<p>Please enter your user Id and property ID</p>
+		Your ID: <input type="text" name="nameID" id="nameID"><br><br>
+		Property ID: <input type="text" name="propertyID" id="propertyID"><br><br>
+		<button type="submit">submit</button>
+		<button type="reset">Reset</button>
+	</form>
+</body>
+</html>
--- src/servLet/rate_check_ID_avalible.java
+++ src/servLet/rate_check_ID_avalible.java
@@ -1,6 +1,7 @@
 package servLet;
 
 import java.io.IOException;
+import java.sql.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,24 +9,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import classes.Property;
 import dao.DBHandler;
 
-@WebServlet(urlPatterns = "/review")
-
-public class rateCheck extends HttpServlet{
-
+@WebServlet(urlPatterns = "/checkIDAva")
+public class rate_check_ID_avalible extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		DBHandler dbHandler = new DBHandler();
-		req.getRequestDispatcher("/rateProperty.jsp").forward(req, resp);
+		
+		
+		req.getRequestDispatcher("/rateMainScreen.jsp").forward(req, resp);
 		dbHandler.closeDB();
-	}
+		}
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO auto-generated method stub
+		// TODO Auto-generated method stub
 		super.doGet(req, resp);
 	}
-
+	
 }
