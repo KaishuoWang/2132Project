37b21abe4b1abb0cf66d0dce88b68c04c2bddb78
RalphHuo <whuo026@uottawa.ca>
2020-04-08 20:16:02 -0400

    guest

--- src/servLet/finishPaymentServlet.java
+++ src/servLet/finishPaymentServlet.java
@@ -19,8 +19,6 @@ public class finishPaymentServlet extends HttpServlet{
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		DBHandler dbHandler = new DBHandler();
-		req.getRequestDispatcher("/lastPage.jsp").forward(req, resp);
-		resp.sendRedirect("paymentPR.html");
 		Payment payment = new Payment(Integer.parseInt(req.getParameter("paymentID")),Integer.parseInt(req.getParameter("hostID")),req.getParameter("paymentType"),Float.parseFloat(req.getParameter("amount")), req.getParameter("status"));
 		if(req.getParameter("paymentID").equals("")) {
 			payment.setPaymentID(0);
@@ -39,6 +37,7 @@ public class finishPaymentServlet extends HttpServlet{
 			}
 		dbHandler.insertePayment(payment);
 		dbHandler.closeDB();
+		req.getRequestDispatcher("/lastPage.jsp").forward(req, resp);
 		}
 	
 	@Override
